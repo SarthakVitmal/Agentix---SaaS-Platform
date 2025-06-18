2f36fecdfbd7d3714fb4ff7f416e7cdcd48c6569
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-06-18 17:02:30 +0530

    feat: implement agent detail view with loading and error states; enhance agents list with row click navigation

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -10,8 +10,10 @@ import { columns } from "../components/columns";
 import EmptyState from "@/components/empty-state";
 import { useAgentsFilters } from "../..//hooks/use-agents-filters";
 import { DataPagination } from "../components/agents-pagination";
+import { useRouter } from "next/navigation";
 
 export const AgentsView = () => {
+    const router = useRouter();
     const [filters, setFilters] = useAgentsFilters();
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -20,7 +22,7 @@ export const AgentsView = () => {
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.item} columns={columns} />
+            <DataTable data={data.item} onRowClick={(row) => router.push(`/agents/${row.id}`)} columns={columns} />
             <DataPagination page={filters.page} totalPages={data.totalPages} onPageChange={(page) => setFilters({page})}/>
             {data.item.length === 0 && (
                 <EmptyState title="Create your first agent" description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call" />
